be0d010ff6e83aa295b55c83ac23c237551613d1
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2022-10-28 15:39:42 +0100

    My first commit

---
+++ myData.js
@@ -0,0 +1,7 @@
+const data = {
+  slackUsername: "Precious Nnorom",
+  backend: true,
+  age: 24,
+  bio: "I am Precious Nnorom, a budding backend developer that is always willing and ready to learn",
+};
+module.exports = { data };
